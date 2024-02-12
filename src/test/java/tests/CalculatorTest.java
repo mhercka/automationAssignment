0170d2f3f1d7aacd3ab931bd89035528632868a9
Martin <martinhercka1@gmail.com>
2024-02-12 22:18:11 +0100

    - edit path to testng.xml - add initialize page only once and remove unecessary lines

--- src/test/java/tests/CalculatorTest.java
+++ src/test/java/tests/CalculatorTest.java
@@ -5,6 +5,7 @@ package tests;
 import io.qameta.allure.Step;
 
 import org.assertj.core.api.Assertions;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.CalculatorPage;
 import selectors.CalculatorPageSelectors;
@@ -12,20 +13,22 @@ import selectors.CalculatorPageSelectors;
 
 public class CalculatorTest extends Setup {
 
- 
+    private CalculatorPage calculatorPage;
+    @BeforeMethod
+    private void setup() {
+        calculatorPage = new CalculatorPage(driver);
+    }
     @Test
     public void displayedNumberTest() {
         verifyIfClickedNumberIsDisplayed();
     }
     @Step("Step 1: Click number '5' and verify if its displayed on screen")
     public void verifyIfClickedNumberIsDisplayed(){
-        CalculatorPage calculatorPage = new CalculatorPage(driver);
         calculatorPage.clickNumber("5");
 
         String text = calculatorPage.getTextFromDisplay(CalculatorPageSelectors.calculatorDisplay);
         Assertions.assertThat(text).isEqualTo("5");
     }
-
     @Test
     public void verifyMultipleResult() {
         multipleNumbers();
@@ -33,7 +36,6 @@ public class CalculatorTest extends Setup {
     }
     @Step("Step 1: Multiple number 5 with 3")
     public void multipleNumbers(){
-        CalculatorPage calculatorPage = new CalculatorPage(driver);
         calculatorPage.clickNumber("5");
         calculatorPage.clickMultipleButton();
         calculatorPage.clickNumber("3");
@@ -41,7 +43,6 @@ public class CalculatorTest extends Setup {
     }
     @Step("Step 2: Verify if result is 15")
     public void verifyResult(){
-        CalculatorPage calculatorPage = new CalculatorPage(driver);
         Assertions.assertThat(calculatorPage.getTextFromDisplay(CalculatorPageSelectors.resultDisplay)).isEqualTo("15");
     }
 }
